db1cb4b1c4fc5b5e7377b087a4cc2ef9559a7e35
rahim jangi <rahimjangi1982@yahoo.com>
2021-04-15 22:45:40 +0200

    tt

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Data
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext(DbContextOptions options):base(options)
+        {
+
+        }
+       
+        public DbSet<Country>Countries{ get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(
+                    new Country
+                    {
+                        Id = 1,
+                        Name = "Italy",
+                        ShortName = "IT"
+                    },
+                    new Country
+                    {
+                        Id = 2,
+                        Name = "Germany",
+                        ShortName = "GM"
+                    },
+                    new Country
+                    {
+                        Id = 3,
+                        Name = "The united state",
+                        ShortName = "USA"
+                    }
+                );
+            builder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "hotel1",
+                    CountryId = 1,
+                    Address = "Fake Address",
+                    Rating = 4.5
+
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "hotel2",
+                    CountryId = 2,
+                    Address = "Fake Address",
+                    Rating = 4.5
+
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "hotel3",
+                    CountryId = 3,
+                    Address = "Fake Address",
+                    Rating = 4.5
+
+                }
+
+                );
+        }
+    }
+}
